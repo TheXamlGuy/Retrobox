61f61c519d8e50f302d0c77f0cb2ebf329d8ba95
Daniel Clark <dan_clark@outlook.com>
2022-12-21 10:14:07 +0000

    Remove dialog button footer from ManageLibrary dialog

--- Avalonia/Retrobox/Views/XboxLibrarySettingView.axaml.cs
+++ Avalonia/Retrobox/Views/XboxLibrarySettingView.axaml.cs
@@ -2,15 +2,14 @@ using Avalonia.Styling;
 using System;
 using Toolkit.Controls.Avalonia;
 
-namespace Retrobox
-{
-    public partial class XboxLibrarySettingView : SettingsExpander, IStyleable
-    {
-        public XboxLibrarySettingView()
-        {
-            InitializeComponent();
-        }
+namespace Retrobox;
 
-        Type IStyleable.StyleKey => typeof(FluentAvalonia.UI.Controls.SettingsExpander);
+public partial class XboxLibrarySettingView : SettingsExpander, IStyleable
+{
+    public XboxLibrarySettingView()
+    {
+        InitializeComponent();
     }
+
+    Type IStyleable.StyleKey => typeof(FluentAvalonia.UI.Controls.SettingsExpander);
 }
